a6ae3a7512480c2a6f2ce6a2246782f9b4008795
7SoKKoS7 <97355065+7SoKKoS7@users.noreply.github.com>
2022-02-24 01:53:20 +0200

    ass menu add start scene index 0

---
+++ Assets/Scripts/MainStart.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainStart : MonoBehaviour
+{
+   
+    public void StartShop()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
